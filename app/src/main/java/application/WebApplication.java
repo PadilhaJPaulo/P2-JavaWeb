944b96ebf58e467c135e560d278f7ec24d394cb8
João Paulo Padilha <joaopaulopadilha04@gmail.com>
2023-06-06 17:57:36 +0000

    new commit final

--- app/src/main/java/application/WebApplication.java
+++ app/src/main/java/application/WebApplication.java
@@ -3,6 +3,7 @@ package application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class WebApplication {
     public static void main(String[] args) {
